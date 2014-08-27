a1b29eeff2331d449bc08520206fcc95b8c4389d
Bharath S <bharathsoftdev@gmail.com>
2014-08-27 19:24:14 +0530

    test#1
    works fine, formatted.

--- pizzaDelivery.cpp
+++ pizzaDelivery.cpp
@@ -5,6 +5,8 @@ using namespace std ;
 int main ( )
 {
     int test = 0 , X = 0, F = 0 , C = 0 ;
+    int LOCost = 0 , OCost = 0 ;
+    int temp1, temp2 ;
     cin >> test ;
     while ( test-- )
     {
@@ -27,6 +29,33 @@ int main ( )
         {
             Total += matF[t1] ;
         }
+        
+        //cout << Total << endl ;
+        
+        LOCost = 0 , OCost = 0 ;
+        
+        // Calculate Least Optimal Cost
+        temp1 = Total ;
+        for ( int t1 = 0 ; t1 < C && temp1 > 0 ; ++t1 )
+        {
+            //cout << matC[t1][0] << endl ;
+            temp2 = temp1/matC[t1][0] ;
+            temp1 = temp1%matC[t1][0] ;
+            LOCost += ( temp2 * matC[t1][1] );
+        }
+        
+        // Calculate Optimal Cost
+        temp1 = Total ;
+        for ( int t1 = C ; t1 >= 0 && temp1 > 0 ; --t1 )
+        {
+            //cout << matC[t1][0] << endl ;
+            temp2 = temp1/matC[t1][0] ;
+            temp1 = temp1%matC[t1][0] ;
+            OCost += ( temp2 * matC[t1][1] );
+        }
+        
+        //cout << LOCost  << ' ' << OCost << ' ' ;
+        cout << (LOCost-OCost) ;
         
         /*cout << endl ;
         for ( int t1 = 0 ; t1 < C ; ++t1 )
